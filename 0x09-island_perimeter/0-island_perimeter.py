74e79ef58783333d41503393ae71f1556467fc16
Babatunde Adeoti <babaolu98@gmail.com>
2024-09-27 08:55:43 +0100

    Perimeter of island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This module implements a function to calculate and island's perimeter """
+
+
+def island_perimeter(grid):
+    """ Calculates perimeter of island found in grid """
+    if not isinstance(grid, list):
+        return -1
+    i_max = len(grid)
+    j_max = len(grid[0])
+
+    per = 0
+    for i in range(i_max):
+        for j in range(j_max):
+            if grid[i][j] == 1:
+                if (i == 0) or (grid[i - 1][j] == 0):
+                    per += 1
+                if (i == i_max - 1) or (grid[i + 1][j] == 0):
+                    per += 1
+                if (j == 0) or (grid[i][j - 1] == 0):
+                    per += 1
+                if (j == j_max - 1) or (grid[i][j - 1] == 0):
+                    per += 1
+    return per
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,1 @@
+Evaluating the Perimeter of an island
